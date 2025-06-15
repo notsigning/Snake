1e305f7bcfc467c5f2745a08f723d70b1c770de7
notsigning <75953880+notsigning@users.noreply.github.com>
2025-06-15 14:42:41 -0700

    Update Snake.java
    More complaints about formatting...
    It's fine now
    removed some print statements because they output dumb messages
    Also added code to prevent overlap between snake and food

--- src/Snake.java
+++ src/Snake.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
-public class Snake extends JComponent{
+public class Snake extends JComponent {
     private ArrayList<Integer> xCoords;
     private ArrayList<Integer> yCoords;
     private int direction; 
@@ -18,21 +18,20 @@ public class Snake extends JComponent{
     private int foodx;
     private int foody;
     int score;
-    public int getDirection(){ //returns direction
+    public int getDirection() { //returns direction
         return direction;
     }
-    public void setDirection(int a){ //changes direction
+    public void setDirection(int a) { //changes direction
         direction = a;
     }
-    public Snake(){ //constructor
+    public Snake() { //constructor
         super();
         reset();
     }
-    public void reset(){ //resets the game
+    public void reset() { //resets the game
         xCoords = new ArrayList<Integer>();
         yCoords = new ArrayList<Integer>();
-        for(int i=5;i<=10;i++)
-        {
+        for(int i=5;i<=10;i++) {
             addPoint(i,7);
         }
         direction = 2;
@@ -40,7 +39,7 @@ public class Snake extends JComponent{
         foody = 7;
         score = 0;
     }
-    public void addPoint(int x,int y){ //adds a snake segment to the arraylist
+    public void addPoint(int x,int y) { //adds a snake segment to the arraylist
         xCoords.add(x);
         yCoords.add(y);
     }
@@ -48,8 +47,8 @@ public class Snake extends JComponent{
     Moves the player forward, and accounts for all actions that may happen, e.g. eating a dot
     Returns true if player is still alive, returns false otherwise
     */
-    public boolean move(){
-        switch (direction){ //adds a segment based on direction
+    public boolean move() {
+        switch (direction) { //adds a segment based on direction
             case 1:
                 xCoords.add(xCoords.get(xCoords.size()-1));
                 yCoords.add(yCoords.get(yCoords.size()-1)-1);
@@ -71,37 +70,39 @@ public class Snake extends JComponent{
         
         //surprisingly efficient algorithm to simulate dot eating
         //if not eating a dot, remove the last segment of the snake
-        if(!(foodx == xCoords.get(xCoords.size()-1) && foody == yCoords.get(yCoords.size()-1))){
+        if(!(foodx == xCoords.get(xCoords.size()-1) && foody == yCoords.get(yCoords.size()-1))) {
             xCoords.remove(0);
             yCoords.remove(0);
         }
-        else{ //is eating a dot
+        else { //is eating a dot
             //regenerate food coordinates
             foodx = (int)(Math.random()*20);
             foody = (int)(Math.random()*15);
+            while(xCoords.indexOf(foodx) > 0 && yCoords.indexOf(foody) > 0) {
+                //regenerate again if there is overlap
+                foodx = (int)(Math.random()*20);
+                foody = (int)(Math.random()*15);
+            }
             score++;
         }
 
         
         //check for death; all output statements are for debugging and may be removed
-        if(overlap()){
-            System.out.println("You committed ouroboros!");
+        if(overlap()) {
             return false;
         }
-        if(19 < xCoords.get(xCoords.size()-1) || xCoords.get(xCoords.size()-1) < 0){ //out of bounds - x
-            System.out.println("Out of bounds!");
+        if(19 < xCoords.get(xCoords.size()-1) || xCoords.get(xCoords.size()-1) < 0) { //out of bounds - x
             return false;
         }
-        else if(14 < yCoords.get(yCoords.size()-1) || yCoords.get(yCoords.size()-1) < 0){  //out of bounds - y
-            System.out.println("Out of bounds!");
+        else if(14 < yCoords.get(yCoords.size()-1) || yCoords.get(yCoords.size()-1) < 0) {  //out of bounds - y
             return false;
         }
         return true;
     }
-    private boolean overlap(){ //see if the snake has collided with itself
-        for(int i=0;i<xCoords.size()-1;i++){ //only need to check if the head is colliding with anything
-            if(xCoords.get(i) == xCoords.get(xCoords.size()-1)){
-                if(yCoords.get(i) == yCoords.get(xCoords.size()-1)){
+    private boolean overlap() { //see if the snake has collided with itself
+        for(int i=0;i<xCoords.size()-1;i++) { //only need to check if the head is colliding with anything
+            if(xCoords.get(i) == xCoords.get(xCoords.size()-1)) {
+                if(yCoords.get(i) == yCoords.get(xCoords.size()-1)) {
                     return true;
                 }
             }
@@ -111,20 +112,20 @@ public class Snake extends JComponent{
 
 
     //main display of the game
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D)g;
         int xintercept = 50,yintercept = 50;
 
         
         //draws a 20 by 15 field
-        for(int i=0;i<20;i++){
-            for(int j=0;j<15;j++){
+        for(int i=0;i<20;i++) {
+            for(int j=0;j<15;j++) {
                 //draw square based on the sum of i and j
                 Rectangle grid = new Rectangle(30*i+xintercept,30*j+yintercept,30,30);
-                if((i+j)%2 == 1){
+                if((i+j)%2 == 1) {
                     g2.setColor(new Color(120,255,120));
                 }
-                else{
+                else {
                     g2.setColor(new Color(50,255,50));
                 }
                 g2.fill(grid);
@@ -135,7 +136,7 @@ public class Snake extends JComponent{
         //draws snake
         g2.setColor(new Color(75,75,255)); 
         //soon to be added: individual head segment implementation
-        for(int i=xCoords.size()-1;i>=0;i--){ //place the snake body
+        for(int i=xCoords.size()-1;i>=0;i--) { //place the snake body
             //later element = earlier created segment
             Rectangle part2 = new Rectangle(30*xCoords.get(i)+xintercept,30*yCoords.get(i)+yintercept,30,30);
             g2.fill(part2);
