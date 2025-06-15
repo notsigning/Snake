5057e6c4eb5202025cb6f24aaf784347a2b8a02a
notsigning <75953880+notsigning@users.noreply.github.com>
2025-06-14 21:40:51 -0700

    Fixing idiocy
    Made changing directions an actual method, saving a lot of redundancy
    
    Like before, people were complaining about formatting, so i fixed that
    
    Also stopped the game before any key or button presses to make it easier to play at times

--- SnakeMain.java
+++ SnakeMain.java
@@ -1,14 +1,19 @@
 import javax.swing.*;
 import java.awt.event.*;
 public class SnakeMain{
-    public static void main(String[] args) throws InterruptedException
-    {
+    static int frames = 0; //frame count
+    static boolean gameStarted = false;
+    static Snake snek = new Snake();
+    public static void switchDirection(int nextDir){
+        gameStarted = true; //start game upon press
+        //only changes direction sometimes
+        if((snek.getDirection()+2)%4 != nextDir) snek.setDirection(nextDir);
+    }
+    public static void main(String[] args) throws InterruptedException{
         //create new window
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000,600);
-        Snake snek = new Snake();
-
 
 
 
@@ -22,9 +27,8 @@ public class SnakeMain{
         //north/up
         ActionListener n = new ActionListener(){
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                if(snek.getDirection()!=3) snek.setDirection(1);
+            public void actionPerformed(ActionEvent e){
+                switchDirection(1);
             }
         };
         up.addActionListener(n);
@@ -34,9 +38,8 @@ public class SnakeMain{
         //south/down
         ActionListener s = new ActionListener(){
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                if(snek.getDirection()!=1) snek.setDirection(3);
+            public void actionPerformed(ActionEvent e){
+                switchDirection(3);
             }
         };
         down.addActionListener(s);
@@ -46,9 +49,8 @@ public class SnakeMain{
         //west/left
         ActionListener w = new ActionListener(){
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                if(snek.getDirection()!=2) snek.setDirection(4);
+            public void actionPerformed(ActionEvent e){
+                switchDirection(4);
             }
         };
         left.addActionListener(w);
@@ -58,9 +60,8 @@ public class SnakeMain{
         //east/right
         ActionListener e = new ActionListener(){
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                if(snek.getDirection()!=4) snek.setDirection(2);
+            public void actionPerformed(ActionEvent e){
+                switchDirection(2);
             }
         };
         right.addActionListener(e);
@@ -71,33 +72,32 @@ public class SnakeMain{
         frame.add(down);
         frame.add(left);
         frame.add(right);
-
+        //That's a lot of lines, I know
 
         //Keybinds
         //with controls to prevent player from moving backwards by accident and dying instantly as a result
         frame.setFocusable(true);
-        KeyListener keyListener = new KeyListener() { //named after a friend who told me to add keybinds
+        KeyListener keyListener = new KeyListener() { //Used to be named after a friend who told me to add keybinds
             @Override
-            public void keyPressed(KeyEvent e)
-            {
+            public void keyPressed(KeyEvent e){
+                gameStarted = true; //start game upon key press
                 int key = e.getKeyCode();
                 System.out.println("Press");
                 if (key == KeyEvent.VK_LEFT) {
-                    if(snek.getDirection()!=2) snek.setDirection(4);
+                    switchDirection(4);
                 }
             
                 if (key == KeyEvent.VK_RIGHT) {
-                    if(snek.getDirection()!=4) snek.setDirection(2);
+                    switchDirection(2);
                 }
             
                 if (key == KeyEvent.VK_UP) {
-                    if(snek.getDirection()!=3) snek.setDirection(1);
+                    switchDirection(1);
                 }
             
                 if (key == KeyEvent.VK_DOWN) {
-                    if(snek.getDirection()!=1) snek.setDirection(3);
+                    switchDirection(3);
                 }
-                //snek.move();
             }
             //basically unused
             @Override
@@ -111,31 +111,31 @@ public class SnakeMain{
         //snake
         frame.add(snek);
         frame.setVisible(true);
-        int delay = 150; // milliseconds
+        int delay = 150; // milliseconds, may be modified
         ActionListener ticker = new ActionListener() {
-            int frames = 0;
             public void actionPerformed(ActionEvent evt) {
-                if (!snek.move()) {
-                    ((Timer) evt.getSource()).stop(); // stop the game if snake dies
-
-                    //prompt user about restarting
-                    if(JOptionPane.showConfirmDialog(frame,"Your score: " + snek.score + "\nDo you want to restart?","You Died!",0) == 0){
-                        //restart game
-                        snek.reset();
-                        ((Timer) evt.getSource()).start();
+                if(gameStarted){
+                    if (!snek.move()) {
+                        ((Timer) evt.getSource()).stop(); // stop the game if snake dies
+                        //prompt user about restarting
+                        if(JOptionPane.showConfirmDialog(frame,"Your score: " + snek.score + "\nDo you want to restart?","You Died!",0) == 0){
+                            //restart game
+                            snek.reset();
+                            gameStarted = false;
+                            ((Timer) evt.getSource()).start();
+                        }
+                        //If not restarting, exit game
+                        else frame.setVisible(false);
+                   }
+                    else {
+                        snek.repaint();
+                        System.out.println(frames); //mostly for debugging purposes, remove as needed
+                        frames++;
                     }
-                    //If not restarting, exit game
-                    else frame.setVisible(false);
-                    
-                }
-                else {
-                    snek.repaint();
-                    System.out.println(frames); //mostly for debugging purposes, remove as needed
-                    frames++;
                 }
             }
         };
-        Timer timer = new Timer(delay, tick);
+        Timer timer = new Timer(delay, ticker);
         timer.start();
     }
 }
